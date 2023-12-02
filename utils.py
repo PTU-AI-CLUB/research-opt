6a004ba160b6b9002c2af9159bbe27afa357c74f
Sathya Krishnan <ts.sathyakrishnan2002@gmail.com>
2023-12-02 23:13:00 +0530

    summarize paper according to titles

--- utils.py
+++ utils.py
@@ -1,20 +1,65 @@
 import requests
-from dotenv import load_dotenv, find_dotenv
 import os
 from scholarly import scholarly
 from typing import Dict, List, Any
-
+import fitz
+from unidecode import unidecode
+from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
 
-API_TOKEN = os.getenv("HUGGINGFACE_API_TOKEN")
 
-API_URL = "https://api-inference.huggingface.co/models/facebook/bart-large-cnn"
-headers = {"Authorization": f"Bearer {API_TOKEN}"}
+class DocumentSummarizer:
 
-def summarize(payload: str) -> str:
-	response = requests.post(API_URL, headers=headers, json=payload)
-	return response.json()[0]["summary_text"]
+    API_TOKEN = os.getenv("HUGGINGFACE_API_TOKEN")
+
+    # API_URL = "https://api-inference.huggingface.co/models/facebook/bart-large-cnn"
+    API_URL = "https://api-inference.huggingface.co/models/Falconsai/text_summarization"
+    headers = {"Authorization": f"Bearer {API_TOKEN}"}
+
+    def __init__(self, path: str) -> None:
+        self.doc = fitz.open(path)
+        self.toc = self.doc.get_toc(simple=True)
+    
+    def _summarize(self, payload: str):
+        response = requests.post(self.API_URL, headers=self.headers, json=payload)
+        return response.json()[0]["summary_text"]
+    
+    def summarize(self) -> str:
+        summarized_doc = {}
+        for i, content in enumerate(self.toc):
+            title = content[1]
+            page_no = content[2]
+
+            if i+1==len(self.toc):
+                text = unidecode(self.doc[page_no-1].get_text())
+                start_idx = text.find(title)
+                content_text = text[len(title)+start_idx:]
+                
+            
+            else:
+                start_idx = unidecode(self.doc[page_no-1].get_text()).find(title)
+                end_idx = unidecode(self.doc[self.toc[i+1][2]-1].get_text()).find(self.toc[i+1][1])
+                if page_no == self.toc[i+1][2]:
+                    content_text = unidecode(self.doc[page_no-1].get_text())[len(title)+start_idx:end_idx]
+                else:
+                    content_text = unidecode(self.doc[page_no-1].get_text())[len(title)+start_idx:] + \
+                                   unidecode(self.doc[self.toc[i+1][2]-1].get_text())[:end_idx]
+            
+            content_text = content_text.replace("\n", " ")
+            summzarized_content_text = ""
+            while len(content_text)>512:
+                summzarized_content_text += self._summarize(content_text[:512])
+                content_text = content_text[512:]
+            summarized_doc[title] = summzarized_content_text
+        
+        summarized_paper = ""
+        for title, content in summarized_doc.items():
+            summarized_paper += title + "\n" + content
+        return summarized_paper
+    
+
+
 
 def get_author_details(auth_name: str) -> Dict[str, Any]:
 	auth_details = scholarly.search_author(auth_name)
