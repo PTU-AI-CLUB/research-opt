cd5fa73d9b836dc2ce05d3854313e37f4290186a
Sathya Krishnan <ts.sathyakrishnan2002@gmail.com>
2023-12-09 18:25:45 +0530

    move to web_dev

--- extension_scripts/popup.js
+++
@@ -1,8 +0,0 @@
-const summarizeButton = document.getElementById("summarize-button");
-
-summarizeButton.addEventListener("click", async () => {
-  console.log("in popup.js");
-  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    chrome.tabs.sendMessage(tabs[0].id, "summarize");
-  });
-});
