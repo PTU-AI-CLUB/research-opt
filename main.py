af6e4555b2fea33df67c480f3b8f819a14a4c6ab
Sathya Krishnan <ts.sathyakrishnan2002@gmail.com>
2023-12-10 17:44:51 +0530

    view functionality

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from website import create_app
+import os
+
 
 app = create_app()
 
--- website/views.py
+++ website/views.py
@@ -1,8 +1,15 @@
 from flask import Blueprint, render_template, request, send_file
+from flask import redirect, url_for
 from utils import DocumentSummarizer, PdfDoc
 import io
+import os
 
 views = Blueprint("views", __name__)
+
+UPLOAD_FOLDER = os.path.join(os.path.dirname(__file__), 'uploads')  # Define the uploads folder
+
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
 
 def summarize_utility(pdf_path):
     ds = DocumentSummarizer(pdf_path)
@@ -28,12 +35,22 @@ def summarize():
     if pdf.filename == '':
         return "No selected file"
     
-    pdf_path = f"./uploads/{pdf.filename}"
+    pdf_path = os.path.join(UPLOAD_FOLDER, pdf.filename)
     pdf.save(pdf_path)
-
     summarized_pdf = summarize_utility(pdf_path)
-    output_pdf = io.BytesIO()
-    summarized_pdf.output(output_pdf)
-    output_pdf.seek(0)
+    summarized_pdf_path = os.path.join(UPLOAD_FOLDER, f"summarized_{pdf.filename.split('.')[0]}.pdf")
+    summarized_pdf.output(summarized_pdf_path)
     print(pdf.filename, pdf_path)
-    return send_file(output_pdf, as_attachment=True, download_name="summarized.pdf")
+    # return redirect(url_for("views.view_pdf", filename=f"summarized_{pdf.filename.split('.')[0]}.pdf"))
+
+    view_link = url_for('views.view_pdf', filename=f"summarized_{pdf.filename.split('.')[0]}.pdf")
+    link_html = f'<a href="{view_link}" target="_blank">View Summarized PDF</a>'
+    
+    return f"Summarization completed! {link_html}"
+
+
+@views.route("/view-pdf/<filename>")
+def view_pdf(filename):
+    print(filename)
+    path = os.path.join(UPLOAD_FOLDER, filename)
+    return send_file(path, mimetype="application/pdf")
